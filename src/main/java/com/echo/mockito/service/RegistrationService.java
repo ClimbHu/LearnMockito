7d5a3c1d7bb7e7481b9f0c7aaf8a18e4c5f66376
ClimbHu <proberj@163.com>
2023-11-06 15:36:34 +0800

    Mockito小demo

---
+++ src/main/java/com/echo/mockito/service/RegistrationService.java
@@ -0,0 +1,7 @@
+package com.echo.mockito.service;
+
+import com.echo.mockito.entity.User;
+
+public interface RegistrationService {
+    User register(String name, String phone) throws Exception;
+}
